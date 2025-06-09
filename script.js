92dd2a4d320844cd7184fc8e38c92d46656137b3
erfan-ne <erfan.neshati@gmail.com>
2025-06-09 16:47:28 +0330

    refactor: add color variable

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ const colorCard = document.querySelector(".color-card")
 const colorCode = document.querySelector(".color-code")
 const copyBtn = document.querySelector("#copy-btn")
 
+let color = null;
+
 generateBtn.addEventListener("click" , () => {
     let red = Math.floor(Math.random() * 255)
     let green = Math.floor(Math.random() * 255)
     let blue = Math.floor(Math.random() * 255)
 
-    colorCode.innerHTML = `rgb(${red},${green},${blue})`
-    colorCard.style.backgroundColor =  `rgb(${red},${green},${blue})`
+    color = `rgb(${red},${green},${blue})`
+
+    colorCode.innerHTML = color
+    colorCard.style.backgroundColor = color
     
 })
 
-copyBtn.addEventListener("click")
+copyBtn.addEventListener("click" , () => {
+    navigator.clipboard.writeText()
+})
